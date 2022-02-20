0d1d22dd9e117728468cf4ec8787b5eb7923dde8
Елизавета Кочкарева <lissakim.1821@gmail.com>
2022-02-20 13:35:12 +0400

    PIbd-22 Kochkareva Laboratornaya 1

---
+++ IPLabs/src/test/java/ru/ulstu/is/IPLabs/IpLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ulstu.is.IPLabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
